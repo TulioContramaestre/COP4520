c44cea161e113d994ef919d011b68db8b687279a
TulioContramaestre <tcontramaestre@knights.ucf.edu>
2023-01-24 03:57:49 -0500

    trying to fix the runnable implementation class

--- Assignment1/Assignment1.java
+++ Assignment1/Assignment1.java
@@ -1,32 +1,52 @@
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Assignment1 
 {
+    private static Counter count = new Counter(1);
+    private static Counter sumAllPrimes = new Counter(0);
+    private static List<Integer> syncprimes = Collections.synchronizedList(new ArrayList<>());
     public static void main(String args[])
     {
         long startTime = System.nanoTime();
         int threadCount = 8;
-        MyRunnables runnable1 = new MyRunnables();
-        Thread thread1 = new Thread(runnable1); 
-        // ExecutorService threads = Executors.newFixedThreadPool(threadCount);
-
-        ArrayList<Integer> primes = new ArrayList<>();
-        long sumAllPrimes = 0;
+        // ArrayList<Integer> primes = new ArrayList<>();
+        // long sumAllPrimes = 0;
         double max = Math.pow(10,8);
 
+        MyRunnables runnable1 = new MyRunnables();
+        ExecutorService pool = Executors.newFixedThreadPool(threadCount);
         for (int i = 2; i < max; i++)
         {
-            if (isPrime(i))
-            {
-                primes.add(i);
-                sumAllPrimes += i;
-            }
+            pool.execute(runnable1);
         }
-        int length = primes.size();
+        pool.shutdown();
+        try {
+
+            pool.awaitTermination(1, TimeUnit.NANOSECONDS);
+        }
+        catch(Exception e)
+        {
+            
+        }
+
+        // }
+
+        // for (int i = 2; i < max; i++)
+        // {
+        //     if (isPrime(i))
+        //     {
+        //         primes.add(i);
+        //         sumAllPrimes += i;
+        //     }
+        // }
+        int length = syncprimes.size();
         System.out.println("Top ten maximum primes: ");
         for (int i = length - 10; i < length; i++)
         {
-            System.out.println(primes.get(i));
+            System.out.println(syncprimes.get(i));
         }
 
         long endTime = System.nanoTime();
@@ -34,8 +54,8 @@ public class Assignment1
         double totalTimeSec  = (double)totalTime / 1000000000.0;
         // System.out.println(primes);
         System.out.println("Execution time: " + (totalTimeSec) + " seconds");
-        System.out.println("total number of primes: " + primes.size());
-        System.out.println("sum of all primes found: " + sumAllPrimes);
+        System.out.println("total number of primes: " + syncprimes.size());
+        System.out.println("sum of all primes found: " + sumAllPrimes.get());
 
     }
 
@@ -60,15 +80,16 @@ public class Assignment1
         @Override
         public void run()
         {
-            Counter count = new Counter(1);
+            // int counter = 2;
             int counter = count.getAndIncrement();
             while (counter < max)
-            {
+            { 
                 if (isPrime(counter))
                 {
-                    primes.add(counter);
-                    sumAllPrimes += i;
+                    syncprimes.add(counter);
+                    sumAllPrimes.getAndAdd(counter);
                 }
+                counter = count.getAndIncrement();
             }
         }
 
@@ -93,6 +114,12 @@ public class Assignment1
 
         public int get() 
         {
+            return value;
+        }
+
+        public synchronized int getAndAdd(int add)
+        {
+            value += add;
             return value;
         }
     }
